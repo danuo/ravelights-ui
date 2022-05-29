aaee75894d99e09f98ce1277773b37ea4840eec8
Daniel <d.brummerloh@gmail.com>
2022-05-29 18:24:24 +0200

    [WIP] visualize available keywords and entries

--- src/components/AvailableGenerators.vue
+++ src/components/AvailableGenerators.vue
@@ -1,7 +1,17 @@
 <template>
+  <div class="q-pa-lg">
+    <q-option-group
+      v-model="patternFilter"
+      :options="parseFilters()"
+      type="checkbox"
+      inline
+    />
+    {{ availableGenerators[0] }}
+  </div>
+
   <div class="q-pa-md row">
     <q-list bordered separator>
-      <q-item v-for="gen in availableGenerators" :key="gen">
+      <q-item v-for="gen in availableGenerators[1]" :key="gen">
         <q-item-section>
           <q-item-label> {{ gen[0] }} </q-item-label>
           <q-item-label caption> {{ gen[1] }}</q-item-label>
@@ -18,12 +28,23 @@ export default {
   setup() {
     return {
       availableGenerators: ref([]),
+      patternFilter: ref([]),
     };
   },
   mounted() {
     this.getAvailableGenerators();
   },
   methods: {
+    parseFilters() {
+      let filters = [];
+      for (var index in this.availableGenerators[0]) {
+        filters.push({
+          label: this.availableGenerators[0][index],
+          value: this.availableGenerators[0][index],
+        });
+      }
+      return filters;
+    },
     getAvailableGenerators() {
       fetch('/api/available_generators')
         .then((responsePromise) => responsePromise.json())
