e3ae52c15add8d56c278c7898e8da10b791f034c
Daniel <d.brummerloh@gmail.com>
2022-05-29 14:39:18 +0200

    improve button layout with col-3 layout css class

--- src/components/PatternComponent.vue
+++ src/components/PatternComponent.vue
@@ -1,75 +1,13 @@
 <template>
-  <div class="q-gutter-sm">
-    <div class="row q-gutter-sm">
+  <div class="row q-col-gutter-md">
+    <div class="col-3" v-for="n in 5" :key="`xs-${n}`">
       <q-btn
         @click="setActivePattern(0)"
-        class="col"
+        class="col-3"
         label="Pattern 0"
         :color="getColor(0)"
         :text-color="getColor(0) == 'white' ? 'black' : 'white'"
-        style="aspect-ratio: 4 / 1"
-      />
-      <q-btn
-        @click="setActivePattern(1)"
-        class="col"
-        label="Pattern 1"
-        :color="getColor(1)"
-        :text-color="getColor(1) == 'white' ? 'black' : 'white'"
-      />
-      <q-btn
-        @click="setActivePattern(2)"
-        class="col"
-        label="Pattern 2"
-        :color="getColor(2)"
-        :text-color="getColor(2) == 'white' ? 'black' : 'white'"
-      />
-    </div>
-    <div class="row q-gutter-sm">
-      <q-btn
-        @click="setActivePattern(3)"
-        class="col"
-        label="Pattern 3"
-        :color="getColor(3)"
-        :text-color="getColor(3) == 'white' ? 'black' : 'white'"
-        style="aspect-ratio: 4 / 1"
-      />
-      <q-btn
-        @click="setActivePattern(4)"
-        class="col"
-        label="Pattern 4"
-        :color="getColor(4)"
-        :text-color="getColor(4) == 'white' ? 'black' : 'white'"
-      />
-      <q-btn
-        @click="setActivePattern(5)"
-        class="col"
-        label="Pattern 5"
-        :color="getColor(5)"
-        :text-color="getColor(5) == 'white' ? 'black' : 'white'"
-      />
-    </div>
-    <div class="row q-gutter-sm">
-      <q-btn
-        @click="setActivePattern(6)"
-        class="col"
-        label="Pattern 6"
-        :color="getColor(6)"
-        :text-color="getColor(6) == 'white' ? 'black' : 'white'"
-        style="aspect-ratio: 4 / 1"
-      />
-      <q-btn
-        @click="setActivePattern(7)"
-        class="col"
-        label="Pattern 7"
-        :color="getColor(7)"
-        :text-color="getColor(7) == 'white' ? 'black' : 'white'"
-      />
-      <q-btn
-        @click="setActivePattern(8)"
-        class="col"
-        label="Pattern 8"
-        :color="getColor(8)"
-        :text-color="getColor(8) == 'white' ? 'black' : 'white'"
+        style="aspect-ratio: 4 / 1; width: 100%"
       />
     </div>
   </div>
