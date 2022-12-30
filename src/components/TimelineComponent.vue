85c86cc0a71381d65445041c9973f2cda2527ba0
Daniel <d.brummerloh@gmail.com>
2022-12-30 12:21:38 +0100

    change set_timeline api call

--- src/components/TimelineComponent.vue
+++ src/components/TimelineComponent.vue
@@ -58,6 +58,7 @@ export default {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
+          action: 'set_timeline',
           timeline_index: this.selectedTimelineIndex,
         }),
       };
