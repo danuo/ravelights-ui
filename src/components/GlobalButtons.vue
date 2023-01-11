4779e24b8ce1174e6eab338e4d1f3b68a51cf02b
Daniel <d.brummerloh@gmail.com>
2023-01-12 00:59:57 +0100

    improve api calls of globalbuttons

--- src/components/GlobalButtons.vue
+++ src/components/GlobalButtons.vue
@@ -1,11 +1,23 @@
 <template>
+  <div class="q-mb-lg">
+    <q-btn-toggle
+      v-model="selectedTargetLevel"
+      toggle-color="primary"
+      :options="[
+        { label: 'Auto', value: 0 },
+        { label: 'Primary', value: 1 },
+        { label: 'Seconday', value: 2 },
+        { label: 'Tertiary', value: 3 },
+      ]"
+    />
+  </div>
   <div class="row q-col-gutter-xs">
     <q-btn
       class="col-4"
       v-for="button in buttons"
       :key="button.name"
       :label="button.name"
-      @click="handleClick(button.action)"
+      @click="handleClick(button)"
     />
   </div>
 </template>
@@ -15,6 +27,7 @@ export default {
   name: 'ButtonGenerator',
   data() {
     return {
+      selectedTargetLevel: 1,
       buttons: [],
     };
   },
@@ -34,9 +47,7 @@ export default {
       const requestOptions = {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          action: action,
-        }),
+        body: JSON.stringify({ ...action, level: this.selectedTargetLevel }),
       };
       fetch('/api', requestOptions)
         .then((responsePromise) => responsePromise)
