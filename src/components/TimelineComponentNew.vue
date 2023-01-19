895197fe63605cece3673f9d44e9933b1fae3570
Daniel <d.brummerloh@gmail.com>
2023-01-19 20:52:44 +0100

    improve styling with q-list bordered separator

--- src/components/TimelineComponentNew.vue
+++ src/components/TimelineComponentNew.vue
@@ -1,21 +1,36 @@
 <template>
-  <div class="q-pa-md" v-if="apiResponse !== null">
-    <div class="row" v-for="idx in names.length" :key="idx">
-      <div class="col-8">
-        <div style="width: 100%; height: 100px" v-html="svgs[idx - 1]" />
-      </div>
-      <div class="col-4">
-        <div class="column">
-          <div class="col">
-            <q-btn :label="names[idx - 1]" style="width: 100%; height: 50px" />
+  <q-list bordered separator v-if="apiResponse !== null">
+    <q-item v-for="idx in names.length" :key="idx">
+      <q-item-section>
+        <q-item-label caption>
+          {{ names[idx - 1] + descriptions[idx - 1] }}
+        </q-item-label>
+        <div class="row">
+          <div class="col-8">
+            <div style="width: 100%; height: 100px" v-html="svgs[idx - 1]" />
           </div>
-          <div class="col">
-            <q-btn :label="names[idx - 1]" style="width: 100%; height: 50px" />
+          <div class="col-4">
+            <div class="column">
+              <div class="col">
+                <q-btn
+                  :label="names[idx - 1]"
+                  style="width: 100%; height: 50px"
+                  color="accent"
+                />
+              </div>
+              <div class="col">
+                <q-btn
+                  :label="names[idx - 1]"
+                  style="width: 100%; height: 50px"
+                  color="primary"
+                />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  </div>
+      </q-item-section>
+    </q-item>
+  </q-list>
 </template>
 
 <script>
