68adbde3bd33fa2d0eb87c20ad44ea93b1ade5c2
Daniel <d.brummerloh@gmail.com>
2023-01-21 18:15:33 +0100

    move GlobalButtons to effect page

--- src/pages/ActionsPage.vue
+++ src/pages/ActionsPage.vue
@@ -1,17 +1,14 @@
 <template>
   <q-page>
-    <GlobalButtons />
     <ColorComponent />
   </q-page>
 </template>
 
 <script>
-import GlobalButtons from 'src/components/Actions.vue';
 import ColorComponent from 'src/components/ColorComponent.vue';
 export default {
   name: 'ActionsPage',
   components: {
-    GlobalButtons,
     ColorComponent,
   },
 };
--- src/pages/EffectPage.vue
+++ src/pages/EffectPage.vue
@@ -1,14 +1,17 @@
 <template>
   <q-page padding>
+    <GlobalButtons />
     <EffectComponent />
   </q-page>
 </template>
 
 <script>
+import GlobalButtons from 'src/components/Actions.vue';
 import EffectComponent from 'src/components/EffectComponent.vue';
 export default {
   name: 'EffectPage',
   components: {
+    GlobalButtons,
     EffectComponent,
   },
 };
