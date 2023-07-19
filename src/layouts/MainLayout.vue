8295395ec56d9b3c3c5afeed98a3a3c51b30113a
Daniel <d.brummerloh@gmail.com>
2023-07-19 11:21:52 +0200

    change tab order in navigation

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,17 +2,17 @@
   <q-layout view="hHh lpR fFf">
     <q-header class="bg-primary text-white" height-hint="50">
       <q-tabs align="left" style="height: 6em">
+        <q-route-tab to="/bpm" name="BPM" icon="alarm" />
         <q-route-tab to="/perform" name="Perform" icon="live_tv" />
+        <q-route-tab to="/trigger" name="Trigger" icon="pattern" />
         <q-route-tab to="/effects" name="Effects" icon="stream" />
         <q-route-tab to="/color" name="Color" icon="palette" />
-        <q-route-tab to="/bpm" name="BPM" icon="alarm" />
         <q-route-tab
           to="/autopilot"
           name="Autopilot"
           icon="motion_photos_auto"
         />
         <q-route-tab to="/timelines" name="Timelines" icon="movie" />
-        <q-route-tab to="/trigger" name="Trigger" icon="pattern" />
       </q-tabs>
     </q-header>
 
