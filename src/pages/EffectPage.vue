9f758fbf238f386053f0547111e95960a489918e
Daniel <d.brummerloh@gmail.com>
2023-08-01 22:37:21 +0200

    move alternate buttons to trigger page

--- src/pages/EffectPage.vue
+++ src/pages/EffectPage.vue
@@ -1,19 +1,16 @@
 <template>
   <q-page padding>
     <EffectList />
-    <GlobalButtons />
     <EffectComponent />
   </q-page>
 </template>
 
 <script>
-import GlobalButtons from "src/components/Actions.vue";
 import EffectComponent from "src/components/EffectComponent.vue";
 import EffectList from "src/components/EffectList.vue";
 export default {
   name: "EffectPage",
   components: {
-    GlobalButtons,
     EffectComponent,
     EffectList,
   },
--- src/pages/TriggerPage.vue
+++ src/pages/TriggerPage.vue
@@ -1,17 +1,17 @@
 <template>
   <q-page padding>
+    <GlobalButtons />
     <TriggerComponent />
   </q-page>
 </template>
 
-<!-- from api we get this -->
-<!-- {'trigger_on_beats': [0], 'trigger_on_quarters': 'A', 'loop_length': 4, 'p': 1.0} -->
-
 <script lang="ts">
-import TriggerComponent from 'src/components/TriggerComponent.vue';
+import TriggerComponent from "src/components/TriggerComponent.vue";
+import GlobalButtons from "src/components/Actions.vue";
 export default {
-  name: 'TriggerPage',
+  name: "TriggerPage",
   components: {
+    GlobalButtons,
     TriggerComponent,
   },
 };
