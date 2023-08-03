ff20060887776487938eb3e6f8d5b96a836da27f
Daniel <d.brummerloh@gmail.com>
2023-08-03 23:38:25 +0200

    improve timelines ui

--- src/components/TimelineComponent.vue
+++ src/components/TimelineComponent.vue
@@ -1,15 +1,39 @@
 <template>
-  <div class="q-pa-md" v-if="apiResponse !== null">
-    <q-chip
-      outline
-      v-for="idx in colors.length"
-      :key="idx"
-      :style="'color: ' + colors[idx - 1]"
-    >
-      {{ "level " + idx }}
-    </q-chip>
-  </div>
-  <q-list bordered separator v-if="apiResponse !== null">
+  <h5 class="text-center q-ma-xs">Active Timeline</h5>
+  <q-list bordered separator v-if="names !== null">
+    <q-item>
+      <q-item-section>
+        <div class="row">
+          <div class="col-8">
+            <q-item-label caption>
+              {{
+                names[active_timeline_index] +
+                " | " +
+                descriptions[active_timeline_index]
+              }}
+            </q-item-label>
+            <div
+              style="width: 100%; height: 100px"
+              v-html="svgs[active_timeline_index]"
+            />
+          </div>
+          <div class="col-4">
+            <div class="column flex-center" style="height: 100%">
+              <div class="col" v-for="idx in colors.length" :key="idx">
+                <q-chip outline :style="'color: ' + colors[idx - 1]">
+                  {{ "level " + idx }}
+                </q-chip>
+              </div>
+            </div>
+          </div>
+        </div>
+      </q-item-section>
+    </q-item>
+  </q-list>
+
+  <h5 class="text-center q-ma-xs">Load Timelines</h5>
+
+  <q-list bordered separator v-if="names !== null">
     <q-item v-for="(e, idx) in names.length" :key="idx">
       <q-item-section>
         <q-item-label caption>
@@ -26,8 +50,8 @@
                   @click="setTimeline(idx, true)"
                   label="load full"
                   style="width: 100%; height: 50px"
-                  :color="idx == selectedTimelineIndex ? 'white' : 'dark'"
-                  :text-color="idx == selectedTimelineIndex ? 'black' : 'white'"
+                  :color="idx == active_timeline_index ? 'white' : 'dark'"
+                  :text-color="idx == active_timeline_index ? 'black' : 'white'"
                 />
               </div>
               <div class="col">
@@ -47,28 +71,28 @@
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "TimelineComponent",
-  data() {
+  setup() {
     return {
-      apiResponse: null,
-      names: null,
-      descriptions: null,
-      svgs: null,
-      colors: null,
-      selectedTimelineIndex: null,
+      names: ref(null),
+      descriptions: ref(null),
+      svgs: ref(null),
+      colors: ref(null),
+      active_timeline_index: ref(null),
     };
   },
   mounted() {
     fetch("/rest")
       .then((responsePromise) => responsePromise.json())
       .then((response) => {
-        this.apiResponse = response;
         this.names = response.meta.timelines.names;
         this.descriptions = response.meta.timelines.descriptions;
         this.svgs = response.meta.timelines.svgs;
         this.colors = response.meta.timelines.colors;
-        this.selectedTimelineIndex = response.active_timeline_index;
+        this.active_timeline_index = response.active_timeline_index;
       })
       .catch((err) => {
         console.log(err);
@@ -76,13 +100,13 @@ export default {
   },
   methods: {
     setTimeline(timeline_index, set_full) {
-      this.selectedTimelineIndex = timeline_index;
+      this.active_timeline_index = timeline_index;
       const requestOptions = {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           action: "set_timeline",
-          timeline_index: this.selectedTimelineIndex,
+          timeline_index: this.active_timeline_index,
           set_full: set_full,
         }),
       };
