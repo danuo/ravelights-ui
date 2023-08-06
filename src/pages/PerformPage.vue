b9819a73963dc71f6f44a48994a40cc546d518ba
Daniel <d.brummerloh@gmail.com>
2023-08-06 23:06:58 +0200

    add manual timeline to perform page

--- src/pages/PerformPage.vue
+++ src/pages/PerformPage.vue
@@ -1,15 +1,18 @@
 <template>
   <q-page padding>
+    <ManualTimelineComponent />
     <GeneratorsComponent />
   </q-page>
 </template>
 
 <script lang="ts">
-import GeneratorsComponent from 'src/components/GeneratorsComponent.vue';
+import GeneratorsComponent from "src/components/GeneratorsComponent.vue";
+import ManualTimelineComponent from "src/components/ManualTimelineComponent.vue";
 export default {
-  name: 'PerformPage',
+  name: "PerformPage",
   components: {
     GeneratorsComponent,
+    ManualTimelineComponent,
   },
 };
 </script>
