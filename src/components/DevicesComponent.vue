250557ef00ea7c087fe3569ff17a1ecc129ee09d
Daniel <d.brummerloh@gmail.com>
2023-08-13 22:15:20 +0200

    ad manual timeline component to top of device page

--- src/components/DevicesComponent.vue
+++ src/components/DevicesComponent.vue
@@ -1,74 +1,78 @@
 <template>
-  <q-list bordered separator>
-    <q-item v-for="(e, idx) in devices.length" :key="idx">
-      <q-item-section>
-        <q-item-label caption style="color: #676767">
-          Device {{ idx }} | lights:{{ devices[idx].n_lights }} | leds:{{
-            devices[idx].n_leds
-          }}
-        </q-item-label>
-        <div class="q-mt-xs row justify-between">
-          <q-btn-toggle
-            v-model="devices[idx]['device_manual_timeline_level']"
-            @click="change_device_settings(idx, 'device_manual_timeline_level')"
-            :options="[
-              { label: 'black', value: 0 },
-              { label: '[1]', value: 1 },
-              { label: '[2]', value: 2 },
-              { label: '[3]', value: 3 },
-              { label: 'none', value: 4 },
-            ]"
-            size="lg"
-          />
-        </div>
-        <div class="q-px-md q-py-md">
+  <div class="q-pt-md">
+    <q-list bordered separator>
+      <q-item v-for="(e, idx) in devices.length" :key="idx">
+        <q-item-section>
           <q-item-label caption style="color: #676767">
-            device_triggerskip
+            Device {{ idx }} | lights:{{ devices[idx].n_lights }} | leds:{{
+              devices[idx].n_leds
+            }}
           </q-item-label>
-          <q-slider
-            @change="change_device_settings(idx, 'device_triggerskip')"
-            v-model="devices[idx]['device_triggerskip']"
-            color="primary"
-            selection-color="secondary"
-            track-size="15px"
-            thumb-size="30px"
-            :min="0"
-            :max="10"
-            snap
-          />
-          <q-item-label caption style="color: #676767">
-            device_frameskip
-          </q-item-label>
-          <q-slider
-            @change="change_device_settings(idx, 'device_frameskip')"
-            v-model="devices[idx]['device_frameskip']"
-            color="primary"
-            selection-color="secondary"
-            track-size="15px"
-            thumb-size="30px"
-            :min="1"
-            :max="10"
-            snap
-          />
-          <q-item-label caption style="color: #676767">
-            device_brightness
-          </q-item-label>
-          <q-slider
-            @change="change_device_settings(idx, 'device_brightness')"
-            v-model="devices[idx]['device_brightness']"
-            color="primary"
-            selection-color="secondary"
-            track-size="15px"
-            thumb-size="30px"
-            :min="0"
-            :max="1"
-            :step="0.1"
-            marker-labels
-          />
-        </div>
-      </q-item-section>
-    </q-item>
-  </q-list>
+          <div class="q-mt-xs row justify-between">
+            <q-btn-toggle
+              v-model="devices[idx]['device_manual_timeline_level']"
+              @click="
+                change_device_settings(idx, 'device_manual_timeline_level')
+              "
+              :options="[
+                { label: 'black', value: 0 },
+                { label: '[1]', value: 1 },
+                { label: '[2]', value: 2 },
+                { label: '[3]', value: 3 },
+                { label: 'none', value: 4 },
+              ]"
+              size="lg"
+            />
+          </div>
+          <div class="q-px-md q-py-md">
+            <q-item-label caption style="color: #676767">
+              device_triggerskip
+            </q-item-label>
+            <q-slider
+              @change="change_device_settings(idx, 'device_triggerskip')"
+              v-model="devices[idx]['device_triggerskip']"
+              color="primary"
+              selection-color="secondary"
+              track-size="15px"
+              thumb-size="30px"
+              :min="0"
+              :max="10"
+              snap
+            />
+            <q-item-label caption style="color: #676767">
+              device_frameskip
+            </q-item-label>
+            <q-slider
+              @change="change_device_settings(idx, 'device_frameskip')"
+              v-model="devices[idx]['device_frameskip']"
+              color="primary"
+              selection-color="secondary"
+              track-size="15px"
+              thumb-size="30px"
+              :min="1"
+              :max="10"
+              snap
+            />
+            <q-item-label caption style="color: #676767">
+              device_brightness
+            </q-item-label>
+            <q-slider
+              @change="change_device_settings(idx, 'device_brightness')"
+              v-model="devices[idx]['device_brightness']"
+              color="primary"
+              selection-color="secondary"
+              track-size="15px"
+              thumb-size="30px"
+              :min="0"
+              :max="1"
+              :step="0.1"
+              marker-labels
+            />
+          </div>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
 </template>
 
 <script>
--- src/pages/DevicesPage.vue
+++ src/pages/DevicesPage.vue
@@ -1,15 +1,19 @@
 <template>
   <q-page padding>
+    <ManualTimelineComponent />
     <DevicesComponent />
   </q-page>
 </template>
 
 <script lang="ts">
 import DevicesComponent from "components/DevicesComponent.vue";
+import ManualTimelineComponent from "src/components/ManualTimelineComponent.vue";
+
 export default {
   name: "AutopilotPage",
   components: {
     DevicesComponent,
+    ManualTimelineComponent,
   },
 };
 </script>
