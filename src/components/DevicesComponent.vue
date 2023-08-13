ebaeec8e1c743bea83c3f62135d542d1ee553250
Daniel <d.brummerloh@gmail.com>
2023-08-13 20:45:35 +0200

    draft for devices display

--- src/components/DevicesComponent.vue
+++ src/components/DevicesComponent.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="q-mt-xs q-mb-lg">hello</div>
+  <q-list bordered separator>
+    <q-item v-for="(e, idx) in devices.length" :key="idx">
+      <q-item-section> test </q-item-section>
+    </q-item>
+  </q-list>
 </template>
 
 <script>
 export default {
-  data() {},
-  mounted() {},
+  data() {
+    return {
+      devices: [],
+    };
+  },
+  mounted() {
+    fetch("/rest/devices")
+      .then((responsePromise) => responsePromise.json())
+      .then((response) => {
+        console.log(response);
+        this.devices = response;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
   methods: {},
 };
 </script>
