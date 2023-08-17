5eaad573ff8d441ce63bdd6fe8e972ce2296c72b
Daniel <d.brummerloh@gmail.com>
2023-08-17 20:49:57 +0200

    ActionButtons: fully remove component

--- src/pages/TriggerPage.vue
+++ src/pages/TriggerPage.vue
@@ -1,20 +1,17 @@
 <template>
   <q-page padding>
     <ManualTimelineComponent />
-    <ActionButtons />
     <TriggerComponent />
   </q-page>
 </template>
 
 <script lang="ts">
 import TriggerComponent from "src/components/TriggerComponent.vue";
-import ActionButtons from "src/components/ActionButtons.vue";
 import ManualTimelineComponent from "src/components/ManualTimelineComponent.vue";
 
 export default {
   name: "TriggerPage",
   components: {
-    ActionButtons,
     TriggerComponent,
     ManualTimelineComponent,
   },
