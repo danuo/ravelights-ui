2add6c260ca7271d22b5ea8961a88ea5822752f8
danuo <anon@anon.com>
2024-02-08 00:34:23 +0100

    remove ManualTimelineComponent from all pages

--- src/pages/DevicesPage.vue
+++ src/pages/DevicesPage.vue
@@ -1,11 +1,9 @@
 <template>
   <q-page padding>
-    <ManualTimelineComponent />
     <DevicesComponent />
   </q-page>
 </template>
 
 <script setup>
 import DevicesComponent from "components/DevicesComponent.vue";
-import ManualTimelineComponent from "src/components/ManualTimelineComponent.vue";
 </script>
--- src/pages/PerformPage.vue
+++ src/pages/PerformPage.vue
@@ -1,11 +1,9 @@
 <template>
   <q-page padding>
-    <ManualTimelineComponent />
     <GeneratorsComponent />
   </q-page>
 </template>
 
 <script setup>
-import ManualTimelineComponent from "src/components/ManualTimelineComponent.vue";
 import GeneratorsComponent from "src/components/GeneratorsComponent.vue";
 </script>
