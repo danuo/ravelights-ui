78b1c821d5b727d7dbb78301d7e1b407786495a1
danuo <anon@anon.com>
2024-02-08 21:22:56 +0100

    add api version mismatch alert

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,15 @@
 <script setup>
 import { useAppStore } from "stores/app-store";
 import { useRouter } from "vue-router";
+import { useQuasar } from "quasar";
 
-const appStore = useAppStore();
-initAppStore();
+const REQUIRED_APP_VERSION = 1;
+
+const $q = useQuasar();
 
 const router = useRouter();
+const appStore = useAppStore();
+initAppStore();
 
 async function initAppStore() {
   await appStore.initAllData();
@@ -19,5 +23,23 @@ async function initAppStore() {
   } else {
     appStore.initSSE();
   }
+  check_api_version();
+}
+
+function check_api_version() {
+  var remote_api_version = appStore.meta["API_VERSION"];
+  const title = "API Version missmatch";
+  const message = `Ravelights Remote API Version: ${remote_api_version}\nWebUI Client API Version: ${REQUIRED_APP_VERSION}`;
+  if (REQUIRED_APP_VERSION != remote_api_version) {
+    alert(title, message);
+  }
+}
+
+function alert(title, message) {
+  $q.dialog({
+    title: title,
+    message: message,
+    persistent: true,
+  });
 }
 </script>
