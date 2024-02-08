1cffc829e5fa435f49f42a674b341a94eefb70ae
danuo <anon@anon.com>
2024-02-08 17:14:34 +0100

    make it slim

--- src/components/ManualTimelineComponent.vue
+++ src/components/ManualTimelineComponent.vue
@@ -1,25 +1,18 @@
 <template>
-  <div class="q-mt-xs row justify-between" v-if="settings !== null">
+  <div class="q-mt-xs-pm" v-if="settings !== null">
     <q-btn-toggle
       v-model="settings.global_manual_timeline_level"
       @click="set_manual_timeline_index()"
-      :toggle-color="settings.use_manual_timeline ? 'primary' : 'dark'"
+      toggle-color="secondary"
+      spread
       :options="[
-        { label: 'black', value: 0 },
+        { label: 'b', value: 0 },
         { label: '1', value: 1 },
         { label: '2', value: 2 },
         { label: '3', value: 3 },
+        { label: 't', value: -1 },
       ]"
       size="lg"
-    />
-    <q-btn-toggle
-      v-model="settings.use_manual_timeline"
-      @click="set_manual_timeline_mode()"
-      toggle-color="primary"
-      :options="[
-        { label: 'manual', value: true },
-        { label: 'timeline', value: false },
-      ]"
     />
   </div>
 </template>
@@ -37,14 +30,6 @@ function set_manual_timeline_index() {
     action: "set_settings",
     global_manual_timeline_level:
       appStore.settings.global_manual_timeline_level,
-    use_manual_timeline: appStore.settings.use_manual_timeline,
-  };
-  axiosPut("/rest/settings", body);
-}
-function set_manual_timeline_mode() {
-  let body = {
-    action: "set_settings",
-    use_manual_timeline: appStore.settings.use_manual_timeline,
   };
   axiosPut("/rest/settings", body);
 }
