60dea735cf90855c35ed78aa395a2b44f0f7dae5
danuo <anon@anon.com>
2024-02-08 18:37:22 +0100

    set undefined timeline to null

--- src/components/ManualTimelineComponent.vue
+++ src/components/ManualTimelineComponent.vue
@@ -10,7 +10,7 @@
         { label: '1', value: 1 },
         { label: '2', value: 2 },
         { label: '3', value: 3 },
-        { label: 't', value: -1 },
+        { label: 't', value: null },
       ]"
       size="lg"
     />
