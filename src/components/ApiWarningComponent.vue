a215d482e8767386aaa7c11aa9a5e0a14286fa01
danuo <anon@anon.com>
2024-02-10 18:56:09 +0100

    add "no connection" message to api warning popup

--- src/components/ApiWarningComponent.vue
+++ src/components/ApiWarningComponent.vue
@@ -5,13 +5,18 @@
         <template v-slot:avatar>
           <q-icon name="sync_problem" size="xl" color="black" />
         </template>
-        <div class="text-h6">API Version Mismatch.</div>
-        <div>
-          Backend Version: <strong>{{ serverVer }}</strong>
-        </div>
-        <div>
-          Frontend Version: <strong>{{ uiVer }}</strong>
-        </div>
+        <template v-if="serverVer == null">
+          <div class="text-h6">Server Connection Failed</div>
+        </template>
+        <template v-if="serverVer != null">
+          <div class="text-h6">API Version Mismatch.</div>
+          <div>
+            Backend Version: <strong>{{ serverVer }}</strong>
+          </div>
+          <div>
+            Frontend Version: <strong>{{ uiVer }}</strong>
+          </div>
+        </template>
         <template v-slot:action>
           <q-btn flat color="white" label="Dismiss" v-close-popup />
         </template>
