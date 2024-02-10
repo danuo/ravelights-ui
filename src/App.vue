166ef49a47949a8096a35ca7ad6e1137dfb4877e
danuo <anon@anon.com>
2024-02-10 15:02:12 +0100

    finalize api popup

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,9 @@
 <template>
-  <q-dialog v-model="popup" persistent>
-    <div>
-      <q-banner class="text-white bg-red">
-        <template v-slot:avatar>
-          <q-icon name="sync_problem" size="xl" color="black" />
-        </template>
-        <div class="text-h6">API Version Mismatch.</div>
-        <div>
-          Backend Version: <strong>{{ SERVER_API_VERSION }}</strong>
-        </div>
-        <div>
-          Frontend Version: <strong>{{ UI_API_VERSION }}</strong>
-        </div>
-        <template v-slot:action>
-          <q-btn flat color="white" label="Dismiss" v-close-popup />
-        </template>
-      </q-banner>
-    </div>
-  </q-dialog>
+  <ApiWarningComponent
+    v-if="showPopup"
+    :ui-ver="UI_API_VERSION"
+    :server-ver="SERVER_API_VERSION"
+  ></ApiWarningComponent>
   <router-view />
 </template>
 
@@ -26,12 +12,15 @@ import { ref } from "vue";
 import { useAppStore } from "stores/app-store";
 import { useRouter } from "vue-router";
 
-const UI_API_VERSION = 1;
-const SERVER_API_VERSION = ref(0);
-const popup = ref(false);
+import ApiWarningComponent from "src/components/ApiWarningComponent.vue";
 
 const router = useRouter();
 const appStore = useAppStore();
+
+const UI_API_VERSION = "1"; // set required version here
+
+const SERVER_API_VERSION = ref(UI_API_VERSION);
+const showPopup = ref(false);
 initAppStore();
 
 async function initAppStore() {
@@ -41,18 +30,14 @@ async function initAppStore() {
     router.push("/");
   } else {
     appStore.initSSE();
-    check_api_version();
+    SERVER_API_VERSION.value = appStore.meta["API_VERSION"];
+    checkApiVersion();
   }
 }
 
-function check_api_version() {
-  SERVER_API_VERSION.value = appStore.meta["API_VERSION"];
-  console.log(SERVER_API_VERSION.value);
-  console.log(UI_API_VERSION);
-  if (SERVER_API_VERSION.value != UI_API_VERSION) {
-    console.log(popup.value);
-    popup.value = true;
-    console.log(popup.value);
+function checkApiVersion() {
+  if (UI_API_VERSION != SERVER_API_VERSION.value) {
+    showPopup.value = true;
   }
 }
 </script>
---
+++ src/components/ApiWarningComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-dialog v-model="showPopup" persistent>
+    <div>
+      <q-banner class="text-white bg-red">
+        <template v-slot:avatar>
+          <q-icon name="sync_problem" size="xl" color="black" />
+        </template>
+        <div class="text-h6">API Version Mismatch.</div>
+        <div>
+          Backend Version: <strong>{{ serverVer }}</strong>
+        </div>
+        <div>
+          Frontend Version: <strong>{{ uiVer }}</strong>
+        </div>
+        <template v-slot:action>
+          <q-btn flat color="white" label="Dismiss" v-close-popup />
+        </template>
+      </q-banner>
+    </div>
+  </q-dialog>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  serverVer: String,
+  uiVer: String,
+});
+
+const showPopup = ref(true);
+</script>
