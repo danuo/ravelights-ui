47bf9f937a40cc829ae7cee1e4db75a8ebb676d5
danuo <anon@anon.com>
2024-02-22 21:41:28 +0100

    cleanup

--- src/components/GeneralSettingsComponent.vue
+++ src/components/GeneralSettingsComponent.vue
@@ -1,7 +1,7 @@
 <template v-if="Object.keys(appStore.settings).length > 0">
   <q-list bordered separator>
     <UniversalControlComponent
-      v-for="button in meta.controls_global_sliders"
+      v-for="button in appStore.meta.controls_global_sliders"
       :key="button.label"
       :button="button"
     />
@@ -12,8 +12,7 @@
 import UniversalControlComponent from "src/components/UniversalControlComponent.vue";
 
 import { useAppStore } from "stores/app-store";
-import { storeToRefs } from "pinia";
 
+// meta is static so no need for refs here
 const appStore = useAppStore();
-const { meta } = storeToRefs(appStore);
 </script>
