367acf488ecc1148fd92a6f998e2945b7e3040ce
danuo <anon@anon.com>
2024-02-22 21:40:27 +0100

    cleanup

--- src/components/GeneralSettingsComponent.vue
+++ src/components/GeneralSettingsComponent.vue
@@ -1,7 +1,7 @@
 <template v-if="Object.keys(appStore.settings).length > 0">
   <q-list bordered separator>
     <UniversalControlComponent
-      v-for="button in appStore.meta.controls_global_sliders"
+      v-for="button in meta.controls_global_sliders"
       :key="button.label"
       :button="button"
     />
@@ -11,15 +11,9 @@
 <script setup>
 import UniversalControlComponent from "src/components/UniversalControlComponent.vue";
 
-import { useAppStore, axiosPut } from "stores/app-store";
+import { useAppStore } from "stores/app-store";
 import { storeToRefs } from "pinia";
 
 const appStore = useAppStore();
-const { settings } = storeToRefs(appStore);
-
-function set_settings(name_slider) {
-  let body = { action: "set_settings" };
-  body[name_slider] = appStore.settings[name_slider];
-  axiosPut("/rest/settings", body);
-}
+const { meta } = storeToRefs(appStore);
 </script>
