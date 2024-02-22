76e750157fd35676424afc3a55ec5de771bf2cdd
danuo <anon@anon.com>
2024-02-22 21:37:43 +0100

    move general settings to audiopage

--- src/pages/AudioPage.vue
+++ src/pages/AudioPage.vue
@@ -2,10 +2,12 @@
   <q-page padding>
     <BpmComponent />
     <AudioSettingsComponent />
+    <GeneralSettingsComponent />
   </q-page>
 </template>
 
 <script setup>
 import BpmComponent from "src/components/BpmComponent.vue";
 import AudioSettingsComponent from "src/components/AudioSettingsComponent.vue";
+import GeneralSettingsComponent from "src/components/GeneralSettingsComponent.vue";
 </script>
--- src/pages/ColorPage.vue
+++ src/pages/ColorPage.vue
@@ -1,11 +1,9 @@
 <template>
   <q-page padding>
-    <!-- <ColorComponent /> -->
-    <GeneralSettingsComponent />
+    <ColorComponent />
   </q-page>
 </template>
 
 <script setup>
 import ColorComponent from "src/components/ColorComponent.vue";
-import GeneralSettingsComponent from "src/components/GeneralSettingsComponent.vue";
 </script>
